20ac6f68bee383bd9cb892129dd4002f5f09d415
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-08 19:12:01 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         </div>
         <div className="flex flex-col align-center">
           <h1 className="text-white font-bold text-7xl mx-auto alfa-slab pt-2 pl-16">A.G.A.B.</h1>
-          <p className="text-black font-bold text-2xl lobster mx-auto pt-2 pb-3">Amics dels Gats i d'altres Animals de Balenyà</p>
+          <p className="text-black font-bold text-xl lobster mx-auto pt-2 pb-3">Amics dels Gats i d'altres Animals de Balenyà</p>
         </div>
         <div className="flex flex-col  flex-start items-center pr-8">
             <a href="#" className="text-lg text-black rounded-md p-1 border-4 border-black lora mb-3 bg-white">bizzum: 689969006</a>
